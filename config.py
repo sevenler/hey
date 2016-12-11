2c68b54951369b1830d1902caac4097f03b8b82d
daniel <daniel.song@zoho.com>
2016-12-11 17:45:07 +0800

    edit deploy dir

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ DEFAULT_FETCH_QUESTION_WITH_USER_RANGE = 100
 
 DEBUG = False
 
-DB_PATH = 'mysql://root:root@localhost/hey?charset=utf8'
+DB_PATH = 'mysql://root:123456@localhost/hey?charset=utf8'
 
-ADDRESS_PORT = 8002
+ADDRESS_PORT = 80
